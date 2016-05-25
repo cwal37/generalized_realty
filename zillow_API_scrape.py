889fc3ed912f43184efa9be9934e479b5fdeb478
CJW <cwal37@gmail.com>
2016-05-24 22:32:16 -0400

    Initializing API stuff

--- zillow_API_scrape.py
+++ zillow_API_scrape.py
@@ -8,3 +8,12 @@ Created on Tue May 24 22:06:13 2016
 import pandas as pd
 import numpy as np
 from API_key import *
+from pyzillow.pyzillow import ZillowWrapper, GetUpdatedPropertyDetails
+
+zillow_data = ZillowWrapper(ZWSID)
+updated_property_details_response = zillow_data.get_updated_property_details(12940850)
+result = GetUpdatedPropertyDetails(updated_property_details_response)
+
+print dir(result)
+
+print result.year_built
